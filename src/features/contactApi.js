4c30c30a0885f3f0d9537addfea94d3341482c3e
mdrony5134 <rony830388@gmail.com>
2025-01-23 18:03:29 +0600

    setup ci/cd pipeline and changed base url

--- src/features/contactApi.js
+++ src/features/contactApi.js
@@ -1,17 +1,16 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const contactApi = createApi({
-  reducerPath: 'contactApi', 
+  reducerPath: "contactApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://christina-pali.vercel.app/api/v1', 
+    baseUrl: "https://api.paligroupservices.org/api/v1",
   }),
   endpoints: (builder) => ({
     submitContactForm: builder.mutation({
       query: (formData) => ({
-        url: '/contact',
-        method: 'POST',
-        body: formData, 
+        url: "/contact",
+        method: "POST",
+        body: formData,
       }),
     }),
   }),
